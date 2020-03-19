9a13f61d139fbd32824cba54f1951daa4c21fee0
Paulo Figueiredo <paulo_jof@live.com>
2020-03-18 23:58:32 -0300

    Consulta simples com query methods

--- src/main/java/com/paulofigueiredo/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/paulofigueiredo/workshopmongo/repositories/PostRepository.java
@@ -1,11 +1,15 @@
 package com.paulofigueiredo.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.paulofigueiredo.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContaining(String text);
 
 }
