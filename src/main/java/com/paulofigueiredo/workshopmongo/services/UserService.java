ebf6a9a35c05875d4d5fb334cfc3c9ffb7de7527
Paulo Figueiredo <paulo_jof@live.com>
2020-03-18 21:50:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/paulofigueiredo/workshopmongo/services/UserService.java
+++ src/main/java/com/paulofigueiredo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paulofigueiredo.workshopmongo.domain.User;
+import com.paulofigueiredo.workshopmongo.dto.UserDTO;
 import com.paulofigueiredo.workshopmongo.exception.ObjectNotFoundException;
 import com.paulofigueiredo.workshopmongo.repositories.UserRepository;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
